a79b1c3f613666f6344233a1a50b9ea8f3003888
Sergey Podkolzin <35434142+exprofesso@users.noreply.github.com>
2021-01-30 01:37:45 +0300

    decompositionUsingMethods Ten

---
+++ src/com/epam/elearn/decompositionUsingMethods/Ten.java
@@ -0,0 +1,30 @@
+package com.epam.elearn.decompositionUsingMethods;
+
+/*
+10. Дано натуральное число N.
+Написать метод(методы) для формирования массива, элементами которого являются цифры числа N.
+ */
+
+import java.util.Scanner;
+public class Ten {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Укажите число для заполнения");
+        String num = scanner.nextLine();
+            array(num);
+    }
+
+
+    public static void array (String n){
+
+        int length = String.valueOf(n).length();
+        long num = Long.parseLong(n);
+        long array[] = new long [length];
+        for(int i = 0; i < array.length;  i++){
+            array[i] = num % 10;
+            num /= 10;
+          System.out.print(array[i] + " ");
+        }
+    }
+}
