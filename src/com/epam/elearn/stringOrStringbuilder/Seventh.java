1417c7f2dd42a6bfa4af37bf07095d462bdc1a15
Sergey Podkolzin <35434142+exprofesso@users.noreply.github.com>
2021-02-09 23:45:37 +0300

    stringOrStringbuilder Seventh full

--- src/com/epam/elearn/stringOrStringbuilder/Seventh.java
+++ src/com/epam/elearn/stringOrStringbuilder/Seventh.java
@@ -8,21 +8,18 @@ package com.epam.elearn.stringOrStringbuilder;
 public class Seventh {
 
     public static void main(String[] args) {
-        String example = " This section    goes into more    detail about how    you should  use   Spring Boot. " +
+        String example = "@This section  uuuuuuu  goes into more    detail about how    you should  use   Spring Boot. " +
                 "It covers topics such as build systems,    auto-configuration, and how to run    your applications. " +
                 "We also    cover some    Spring Boot best practices.    Although    there is    nothing    " +
                 "particularly special about Spring Boot (it is just another    library that    you can consume),    " +
                 "there    are    a few    recommendations    that, " +
-                "when followed, make your    development process    a little easier   .";
+                "when followed, make your    development process    a little easier  @ .";
 
         System.out.println(space(example));
-
     }
-    
 
 
-
-
+    // удаление всех пробелов
     public static String space(String a) {
 
         StringBuilder stringBuilder = new StringBuilder();
@@ -30,6 +27,19 @@ public class Seventh {
         for (int i = 0; i < a.length(); i++) {
             if (a.charAt(i) != ' ') {
                 stringBuilder.append(a.charAt(i));
+            }
+        }
+
+
+        // Удаление всех повторений
+        for (int i = 0; i < stringBuilder.length(); i++) {
+            char c1 = stringBuilder.charAt(i);
+            for (int j = stringBuilder.length() - 1; j > i; j--) {
+                char c2 = stringBuilder.charAt(j);
+                if (c1 == c2) {
+
+                    stringBuilder.deleteCharAt(j);
+                }
             }
         }
         String str = stringBuilder.toString();
