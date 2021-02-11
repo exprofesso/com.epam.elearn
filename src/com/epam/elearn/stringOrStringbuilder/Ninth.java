d7671b6fb513c92cc54f06f4f9b3d627e5b754e1
Sergey Podkolzin <35434142+exprofesso@users.noreply.github.com>
2021-02-11 23:27:09 +0300

    stringOrStringbuilder Ninth

--- src/com/epam/elearn/stringOrStringbuilder/Ninth.java
+++ src/com/epam/elearn/stringOrStringbuilder/Ninth.java
@@ -4,4 +4,41 @@ package com.epam.elearn.stringOrStringbuilder;
 // Учитывать только английские буквы.
 
 public class Ninth {
+
+    public static void main(String[] args) {
+
+        String example = "Do You ever make phone calls or leave voicemail messages in English";
+
+        System.out.println(little(example));
+        System.out.println(big(example));
+
+    }
+
+    public static int little(String a) {
+        int count = 0;
+
+        for (int i = 0; i < a.length(); i++) {
+            char letter = a.charAt(i);
+            if (letter > 96 && letter < 123) {
+                count++;
+            }
+        }
+        System.out.print("В данном тексте строчных букв ");
+        return count;
+    }
+
+    public static int big(String a) {
+
+        int count = 0;
+
+        for (int i = 0; i < a.length(); i++) {
+            char letter = a.charAt(i);
+            if (letter > 65 && letter < 90) {
+                count++;
+            }
+        }
+        System.out.print("В данном тексте заглавных букв ");
+
+        return count;
+    }
 }
