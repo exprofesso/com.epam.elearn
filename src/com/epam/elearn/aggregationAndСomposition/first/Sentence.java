ab29256ba062d8e9befa905b9e6caaf00fbbe9e6
Sergey Podkolzin <35434142+exprofesso@users.noreply.github.com>
2021-03-13 16:26:00 +0300

    Aggregation And Сomposition first task create class

---
+++ src/com/epam/elearn/aggregationAndСomposition/first/Sentence.java
@@ -0,0 +1,17 @@
+package com.epam.elearn.aggregationAndСomposition.first;
+
+public class Sentence {
+    private String sentense;
+
+    public Sentence(String sentense) {
+        this.sentense = sentense;
+    }
+
+    public String getSentense() {
+        return sentense;
+    }
+
+    public void setSentense(String sentense) {
+        this.sentense = sentense;
+    }
+}
---
+++ src/com/epam/elearn/aggregationAndСomposition/first/Word.java
@@ -0,0 +1,15 @@
+package com.epam.elearn.aggregationAndСomposition.first;
+
+public class Word {
+    private String word;
+    public Word(String word) {
+        this.word = word;
+    }
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+}
