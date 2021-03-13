773d99d6df3e67e6cb275d48f4c9bd62d576af54
Sergey Podkolzin <35434142+exprofesso@users.noreply.github.com>
2021-03-13 22:37:04 +0300

    Aggregation And Сomposition first task word, text, sentence

--- src/com/epam/elearn/aggregationAndСomposition/first/Text.java
+++ src/com/epam/elearn/aggregationAndСomposition/first/Text.java
@@ -6,4 +6,30 @@ package com.epam.elearn.aggregationAndСomposition.first;
  */
 
 public class Text {
+    private String header;
+    private String body = "";
+
+    public Text(Word word) {
+        header = word.getWord();
+    }
+
+    public Text(Sentence sentence) {
+        header = sentence.getSentense();
+    }
+
+    public String getHeader() {
+        return header;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void addBody(Word word) {
+        body += " " + word.getWord();
+    }
+
+    public void addBody(Sentence sentence) {
+        body += " " + sentence.getSentense();
+    }
 }
--- src/com/epam/elearn/aggregationAndСomposition/first/Word.java
+++ src/com/epam/elearn/aggregationAndСomposition/first/Word.java
@@ -2,14 +2,20 @@ package com.epam.elearn.aggregationAndСomposition.first;
 
 public class Word {
     private String word;
+
     public Word(String word) {
         this.word = word;
     }
+
     public String getWord() {
         return word;
     }
 
     public void setWord(String word) {
         this.word = word;
+    }
+
+    public String toString() {
+        return word;
     }
 }
