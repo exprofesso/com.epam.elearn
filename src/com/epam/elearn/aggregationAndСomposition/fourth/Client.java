f832f28242800e9ff8215edc7b135a02004378da
Sergey Podkolzin <35434142+exprofesso@users.noreply.github.com>
2021-03-19 17:01:29 +0300

    fourth create client

--- src/com/epam/elearn/aggregationAndСomposition/fourth/Client.java
+++ src/com/epam/elearn/aggregationAndСomposition/fourth/Client.java
@@ -1,6 +1,7 @@
 package com.epam.elearn.aggregationAndСomposition.fourth;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 
 public class Client {
     private String firstName;
@@ -23,7 +24,7 @@ public class Client {
         return String.format("Баланс всех считов в банке " + balance);
     }
 
-    public String getPossitivBalance() {
+    public String getPositiveBalance() {
         double balance = 0;
         for (int i = 0; i < accounts.size(); i++) {
             if (accounts.get(i).getBalance() > 0) {
@@ -31,5 +32,60 @@ public class Client {
             }
         }
         return String.format("Баланс положительных считов в банке " + balance);
+    }
+
+    public String getNegativeBalance() {
+        double balance = 0;
+        for (int i = 0; i < accounts.size(); i++) {
+            if (accounts.get(i).getBalance() < 0) {
+                balance -= accounts.get(i).getBalance();
+            }
+        }
+        return String.format("Баланс отрицательных считов в банке " + balance);
+    }
+
+    public void sortBalance() {
+        accounts.sort(Comparator.comparing(Account::getBalance));
+    }
+
+    public Account getAccount(int id) {
+        for (int i = 0; i < accounts.size(); i++) {
+            if (accounts.get(i).getId() == id) {
+                return accounts.get(i);
+            }
+        }
+        return null;
+    }
+
+    public void openAccount(Account account) {
+        accounts.add(account);
+    }
+
+    public void blockAccount(int id) {
+        for (int i = 0; i < accounts.size(); i++) {
+            if (accounts.get(i).getId() == id) {
+                accounts.get(i).block();
+            }
+        }
+    }
+
+    public void unBlockAccount(int id) {
+        for (int i = 0; i < accounts.size(); i++) {
+            if (accounts.get(i).getId() == id) {
+                accounts.get(i).unblock();
+            }
+        }
+    }
+
+    public String getInfoAccount() {
+        String info = "";
+        for (int i = 0; i < accounts.size(); i++) {
+            info += String.format("Данные по аккаунтам" + accounts.get(i).getId(), accounts.get(i).getBalance(), accounts.get(i).getStatus());
+        }
+        return String.format(info);
+    }
+
+    public String getPassID() {
+        return passID;
     }
 }
