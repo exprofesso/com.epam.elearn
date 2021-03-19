f078fb8252b6ff0eecc408d3ea8dff4d8b5e1e0b
Sergey Podkolzin <35434142+exprofesso@users.noreply.github.com>
2021-03-19 16:12:34 +0300

    fourth create getBalance

--- src/com/epam/elearn/aggregationAndСomposition/fourth/Client.java
+++ src/com/epam/elearn/aggregationAndСomposition/fourth/Client.java
@@ -1,4 +1,35 @@
 package com.epam.elearn.aggregationAndСomposition.fourth;
 
+import java.util.ArrayList;
+
 public class Client {
+    private String firstName;
+    private String second;
+    private String passID;
+    private ArrayList<Account> accounts;
+
+    public Client(String firstName, String second, String passID) {
+        this.firstName = firstName;
+        this.second = second;
+        this.passID = passID;
+        accounts = new ArrayList<>();
+    }
+
+    public String getBalance() {
+        double balance = 0;
+        for (int i = 0; i < accounts.size(); i++) {
+            balance += accounts.get(i).getBalance();
+        }
+        return String.format("Баланс всех считов в банке " + balance);
+    }
+
+    public String getPossitivBalance() {
+        double balance = 0;
+        for (int i = 0; i < accounts.size(); i++) {
+            if (accounts.get(i).getBalance() > 0) {
+                balance += accounts.get(i).getBalance();
+            }
+        }
+        return String.format("Баланс положительных считов в банке " + balance);
+    }
 }
