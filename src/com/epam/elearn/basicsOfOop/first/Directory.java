ba0efbe8caa0fcc11387179a698fe5dfdd1bd6ef
Sergey Podkolzin <35434142+exprofesso@users.noreply.github.com>
2021-03-28 22:58:02 +0300

    Basic of OOP first create

---
+++ src/com/epam/elearn/basicsOfOop/first/Directory.java
@@ -0,0 +1,4 @@
+package com.epam.elearn.basicsOfOop.first;
+
+public class Directory {
+}
---
+++ src/com/epam/elearn/basicsOfOop/first/File.java
@@ -0,0 +1,9 @@
+package com.epam.elearn.basicsOfOop.first;
+
+public class File {
+    private String name;
+
+    public File(String name){
+        this.name = name;
+    }
+}
