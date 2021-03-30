0783cbdc6f742303821a0425c31cf1a514b65c7f
Sergey Podkolzin <35434142+exprofesso@users.noreply.github.com>
2021-03-30 21:19:53 +0300

    Basic of OOP first text upgrade

--- src/com/epam/elearn/basicsOfOop/first/Text.java
+++ src/com/epam/elearn/basicsOfOop/first/Text.java
@@ -1,4 +1,20 @@
 package com.epam.elearn.basicsOfOop.first;
 
 public class Text {
+    private String text;
+
+    public Text(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void addText(String text) {
+        StringBuilder reader = new StringBuilder(new StringBuilder(this.text));
+        reader.append(' ');
+        reader.append(text);
+        this.text = reader.append(text).toString();
+    }
 }
