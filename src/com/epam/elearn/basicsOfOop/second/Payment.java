ff916844915396a207f819b47c9142560c12c63e
Sergey Podkolzin <35434142+exprofesso@users.noreply.github.com>
2021-04-06 20:26:59 +0300

    Basic of OOP second create product + receipt

--- src/com/epam/elearn/basicsOfOop/second/Payment.java
+++ src/com/epam/elearn/basicsOfOop/second/Payment.java
@@ -1,4 +1,61 @@
 package com.epam.elearn.basicsOfOop.second;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Payment {
+    private Receipt receipt;
+    private double balance;
+
+
+    public Payment(Product... product) {
+        ArrayList<Product> productArrayList = new ArrayList<>();
+        Collections.addAll(productArrayList, product);
+        receipt = new Receipt(productArrayList);
+        int temp = (int) receipt.getBalance() * 100;
+        balance = (double) temp / 100;
+    }
+
+
+    public static class Receipt {
+        private ArrayList<Product> products;
+        private double balance;
+
+        public Receipt(ArrayList<Product> products) {
+            this.products = products;
+            balance = getTotalPrice();
+        }
+
+        public double getTotalPrice() {
+            double totalPrice = 0.0;
+            for (Product product : products) {
+                totalPrice += product.getPrice();
+            }
+            return totalPrice;
+        }
+
+        public void addReceipt(Product product) {
+            products.add(product);
+        }
+
+        public void delReceipt(Product product) {
+            products.remove(product);
+        }
+
+        public double getBalance() {
+            return balance;
+        }
+
+        public void setBalance(double balance) {
+            this.balance = balance;
+        }
+
+        @Override
+        public String toString() {
+            return "Receipt " +
+                    "products = " + products +
+                    ", balance = " + balance;
+        }
+    }
 }
+
--- src/com/epam/elearn/basicsOfOop/second/Product.java
+++ src/com/epam/elearn/basicsOfOop/second/Product.java
@@ -1,4 +1,45 @@
 package com.epam.elearn.basicsOfOop.second;
 
 public class Product {
+    private String name;
+    private int code;
+    private int price;
+
+    public Product(String name, int code, int price) {
+        this.name = name;
+        this.code = code;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Payment " +
+                " name = " + name + ", code = " + code +
+                ", price = " + price + " руб.";
+    }
 }
