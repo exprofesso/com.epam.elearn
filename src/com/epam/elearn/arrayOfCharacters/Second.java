8f4d9c0a388ce1e2f43f9a8bacc5f9b45a92eb4a
Sergey Podkolzin <35434142+exprofesso@users.noreply.github.com>
2021-04-25 12:04:53 +0300

    edit second class

--- src/com/epam/elearn/arrayOfCharacters/Second.java
+++ src/com/epam/elearn/arrayOfCharacters/Second.java
@@ -9,13 +9,16 @@ public class Second {
         String text_replace = "";
 
 //        String letter = text.replace("word", "letter");
-//
 //        System.out.println(letter);
+        
         String word = "word";
         String letter = "letter";
 
         if (text.length() == word.length()) {
-            if ((text.charAt(0) == word.charAt(0)) && (text.charAt(1) == word.charAt(1)) && (text.charAt(2) == word.charAt(2)) && (text.charAt(3) == word.charAt(3))) {
+            if ((text.charAt(0) == word.charAt(0)) &&
+                    (text.charAt(1) == word.charAt(1)) &&
+                    (text.charAt(2) == word.charAt(2)) &&
+                    (text.charAt(3) == word.charAt(3))) {
                 text_replace += letter;
             } else {
                 text_replace = text;
@@ -24,7 +27,10 @@ public class Second {
             text_replace = "Тут менять нечего";
         } else {
             for (int i = 0; i < text.length() - word.length(); i++) {
-                if ((text.charAt(i) == word.charAt(0)) && (text.charAt(i + 1) == word.charAt(1)) && (text.charAt(i + 2) == word.charAt(2)) && (text.charAt(i + 3) == word.charAt(3))) {
+                if ((text.charAt(i) == word.charAt(0)) &&
+                        (text.charAt(i + 1) == word.charAt(1)) &&
+                        (text.charAt(i + 2) == word.charAt(2)) &&
+                        (text.charAt(i + 3) == word.charAt(3))) {
                     text_replace += letter;
                     i = i + word.length() - 1;
 
