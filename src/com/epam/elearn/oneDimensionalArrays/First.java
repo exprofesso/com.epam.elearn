14063c4f701b6eb445ec880d2e774aaf0f084040
sergey <vitebsk12@mail.ru>
2021-09-01 20:30:08 +0300

    edit first task add Scanner

--- src/com/epam/elearn/oneDimensionalArrays/First.java
+++ src/com/epam/elearn/oneDimensionalArrays/First.java
@@ -1,16 +1,23 @@
 package com.epam.elearn.oneDimensionalArrays;
 
 import java.util.Random;
+import java.util.Scanner;
 
 // 1. В массив A [N] занесены натуральные числа. Найти сумму тех элементов, которые кратны данному К.
 public class First {
 
     public static void main(String[] args) {
-        int[] arr = generateArray(10000);
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println(" Укажите натуральное число");
+        int n = scanner.nextInt();
+
+        System.out.println("Укажите второе натуральное число");
+        int k = scanner.nextInt();
+        int[] arr = generateArray(n);
         for (int i = 0; i < arr.length; i++) {
             System.out.println(arr[i]);
         }
-        int k = 5;
         int sum = 0;
         for (int a : arr) {
             if (a % k == 0) {
