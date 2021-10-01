a4dd1467a3b9edb9c2179a161d3f83a3251dfc5a
Sergey Podkolzin <35434142+exprofesso@users.noreply.github.com>
2021-10-01 23:30:27 +0300

    edit 47 task.

--- src/com/epam/elearn/decompositionUsingMethods/Fourteenth.java
+++ src/com/epam/elearn/decompositionUsingMethods/Fourteenth.java
@@ -12,21 +12,71 @@ import java.util.Scanner;
 public class Fourteenth {
     private static int n = 0;
 
+    //    public static void main(String[] args) {
+//
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Введите K: ");
+//        int k = scanner.nextInt();
+//        decomposeNumbersOnDigits(k);
+//    }
+//
+//
+//    public static void decomposeNumbersOnDigits(int k) {
+//        int mass[] = new int[k];
+//        for (int i = 0, j = 0; i < mass.length; i++, j++) {
+//            mass[i] = j;
+//        }
+//        int mas2[] = new int[10];
+//        //разложение числа на цифры
+//        for (int i = 0; i < mass.length; i++) {
+//            int buf1 = mass[i];
+//            while (buf1 >= 1) {
+//                int buf2 = buf1 - ((buf1 / 10) * 10);
+//                buf1 = buf1 / 10;
+//                mas2[n] = buf2;
+//                //счет разрядности числа
+//                n++;
+//            }
+//            //вызов метода сравнения числа массива и числа, вычисленного по алгоритму
+//            if (sum(mas2, n) == mass[i]) {
+//                System.out.println(mass[i]);
+//            }
+//            n = 0;
+//        }
+//    }
+//
+//
+//    //реализация алгоритма согласно примера 153=1*1*1+5*5*5+3*3*3=1+125+27
+//    public static int sum(int mas2[], int n) {
+//        int totalSum = 0;
+//        for (int j = 0, n2 = n; j < n; j++) {
+//            int level = 1;
+//            while (n2 > 0) {
+//                level = level * mas2[j];
+//                n2--;
+//            }
+//            n2 = n;
+//            totalSum = totalSum + level;
+//
+//        }
+//        return totalSum;
+//    }
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите K: ");
         int k = scanner.nextInt();
 
-        decomposition(k);
-    }
-
-
-    public static void decomposition(int k) {
         int mass[] = new int[k];
         for (int i = 0, j = 0; i < mass.length; i++, j++) {
             mass[i] = j;
         }
+
+        decomposeNumbersOnDigits(mass);
+    }
+
+
+    public static int[] decomposeNumbersOnDigits(int[] mass) {
 
         int mas2[] = new int[10];
         //разложение числа на цифры
@@ -39,12 +89,13 @@ public class Fourteenth {
                 //счет разрядности числа
                 n++;
             }
-            //вызов метода сравнения числа массива и числа, вычисленного по алгоритму
+            //      вызов метода сравнения числа массива и числа, вычисленного по алгоритму
             if (sum(mas2, n) == mass[i]) {
                 System.out.println(mass[i]);
             }
             n = 0;
         }
+        return mas2;
     }
 
 
@@ -63,5 +114,4 @@ public class Fourteenth {
         }
         return totalSum;
     }
-
 }
